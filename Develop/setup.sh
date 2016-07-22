0d27200d0212482348061cfef5bdca16c9e0cb4a
Corey Maylone <Corey@livio.io>
2016-07-22 10:50:48 -0400

    Fixed issues where Core TCP listener would not start. Works on OSX with latest Docker

--- Develop/setup.sh
+++ Develop/setup.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
+#
+# Replaces the IP address in smartDeviceLink.ini with the machine's IP address and then runs SmartDeviceLink Core.
+# The IP is required to be replaced for the websocket connection with the HMI to function.
 
-# This script replaces the IP address in smartDeviceLink.ini with the machine's IP address and then runs SmartDeviceLink Core.
-# The IPis required to be replaced for the websocket connection with the HMI to function.
+# Get the machine's IP address
+IP="$(ip addr show ${CORE_NETWORK_INTERFACE} | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)"
 
-# # Get the machine's IP address
-IP="$(ip addr show ${SDL_CORE_NETWORK_INTERFACE} | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)"
-echo "Changing smartDeviceLink.ini HMI ServerAddress to ${SDL_CORE_IP}"
+#######################################
+# Replaces text within a specified file
+#######################################
+function replace {
+    target=$1
+    value=$2
+    file=$3
+
+    echo "${file} - Replacing ${target} with ${value}"
+    perl -pi -e 's/'${target}'/'${value}'/g' ${file}
+}
 
 # Replace the IP address in smartDeviceLink.ini with the machines IP address
-perl -pi -e 's/127.0.0.1/'$IP'/g' /usr/sdl/bin/smartDeviceLink.ini
-
-echo "Changing Flags.js HMI ServerAddress to ${HMI_WEBSOCKET_ADDRESS}"
+replace "127.0.0.1" ${IP} ${PWD}/smartDeviceLink.ini
 
 # Replace IP and Port in flags file to match the machine's IP address
-perl -pi -e 's/127.0.0.1:8087/'$HMI_WEBSOCKET_ADDRESS'/g' /var/www/app/Flags.js
+replace "127.0.0.1:8087" ${HMI_WEBSOCKET_ADDRESS} ${HMI_CWD}/app/Flags.js
+
+# Set location of SDL Core Binaries
+export LD_LIBRARY_PATH=${PWD}
 
 # Start supervisord
 /usr/bin/supervisord
